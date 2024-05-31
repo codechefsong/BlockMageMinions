593ffc158f0af0915cff1a443bbc9db2bf7b1412
Codechef Song <133816663+codechefsong@users.noreply.github.com>
2024-05-31 15:23:22 -0400

    change the home page

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,65 +1,56 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import type { NextPage } from "next";
-import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
-  const { address: connectedAddress } = useAccount();
-
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold">Block Mage Minions</span>
           </h1>
-          <div className="flex justify-center items-center space-x-2">
-            <p className="my-2 font-medium">Connected Address:</p>
-            <Address address={connectedAddress} />
+          <center>
+            <Image
+              className="mb-3"
+              src="https://olive-exceptional-viper-654.mypinata.cloud/ipfs/QmZx4FuZqqSQ9f3MMnsX46ts4RXufRSMAH2byyYy9bqPkH"
+              width={150}
+              height={150}
+              alt="Minions"
+            />
+          </center>
+
+          <p className="text-center text-lg mb-0">Train your minions to become the best</p>
+          <div className="flex justify-center mb-2">
+            <Link
+              href="/explore"
+              passHref
+              className=" py-2 px-16 mb-1 mt-3 bg-green-500 rounded baseline hover:bg-green-400 disabled:opacity-50"
+            >
+              Get Started
+            </Link>
           </div>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
         </div>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
+        <div className="flex-grow bg-blue-200 w-full mt-16 px-8 py-12">
+          <div className="text-center">
+            <h2 className="mt-3 text-4xl mb-5">Gameplay</h2>
+          </div>
+          <div className="flex justify-center">
+            <div className="w-[700px]">
+              <ul className="list-disc text-xl" style={{ width: "600px" }}>
+                <li>Create BlockMage Minions</li>
+                <li>Gather resources</li>
+                <li>Train your Minions</li>
+                <li>Buy potion to make your Minions stronger</li>
+                <li>Buy food to recover your stamina points</li>
+                <li>Each actions for minions cost SP</li>
+                <li>Gather resources and sell them for Rune Credits</li>
+                <li>Battle thief to earn Rune Credits</li>
+              </ul>
             </div>
           </div>
         </div>
