7aa3fc0be86e6d14558ecaf5ba739a34efa0a93e
Codechef Song <133816663+codechefsong@users.noreply.github.com>
2024-06-27 17:53:59 -0400

    update Rune Tree detail page

--- packages/nextjs/app/runetree/tree/[contractaddress]/page.tsx
+++ packages/nextjs/app/runetree/tree/[contractaddress]/page.tsx
@@ -1,14 +1,47 @@
 "use client";
 
 import { formatEther } from "viem";
+import { useAccount } from "wagmi";
 import { BackButton } from "~~/components/ui/BackButton";
 import { useScaffoldReadContract, useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
 
 const RuneTree = ({ params }: { params: { contractaddress: string } }) => {
+  const { address } = useAccount();
+
   const { data: runeTrees } = useScaffoldReadContract({
     contractName: "Game",
     functionName: "getUserRuneTree",
     args: [params.contractaddress],
+  });
+
+  const { data: tbaAddress } = useScaffoldReadContract({
+    contractName: "Game",
+    functionName: "getActiveMinion",
+    args: [address],
+  });
+
+  const { data: sp } = useScaffoldReadContract({
+    contractName: "StaminaPoint",
+    functionName: "balanceOf",
+    args: [tbaAddress],
+  });
+
+  const { data: mp } = useScaffoldReadContract({
+    contractName: "MagicPoint",
+    functionName: "balanceOf",
+    args: [tbaAddress],
+  });
+
+  const { data: dp } = useScaffoldReadContract({
+    contractName: "DefensePoint",
+    functionName: "balanceOf",
+    args: [tbaAddress],
+  });
+
+  const { data: usedsp } = useScaffoldReadContract({
+    contractName: "Game",
+    functionName: "getStaminaPointsLeft",
+    args: [tbaAddress],
   });
 
   const { writeContractAsync: Game } = useScaffoldWriteContract("Game");
@@ -22,6 +55,17 @@ const RuneTree = ({ params }: { params: { contractaddress: string } }) => {
             <span className="block text-3xl mb-2">Rune Tree</span>
           </h1>
           <p className="text-gray-500">Earn Rune Credits for attacking this Rune Tree</p>
+        </div>
+
+        <div>
+          <p className="mr-2">
+            Amount: {parseFloat(formatEther(runeTrees?.amount || 0n))}
+            <span className="font-bold ml-1">RC</span>
+          </p>
+          <p className="mr-2">
+            Defense Point: {parseFloat(formatEther(runeTrees?.defensePoint || 0n))}
+            <span className="font-bold ml-1">DP</span>
+          </p>
         </div>
 
         <button
@@ -40,15 +84,26 @@ const RuneTree = ({ params }: { params: { contractaddress: string } }) => {
           Attack
         </button>
 
-        <div>
-          <p className="mr-2">
-            Amount: {parseFloat(formatEther(runeTrees?.amount || 0n))}
-            <span className="font-bold ml-1">RC</span>
-          </p>
-          <p className="mr-2">
-            Defense Point: {parseFloat(formatEther(runeTrees?.defensePoint || 0n))}
+        <div className="text-xl mt-5">
+          Stamina Point: {usedsp?.toString()} {" / "}
+          <div className="inline-flex items-center justify-center">
+            {parseFloat(formatEther(sp || 0n))}
+            <span className="font-bold ml-1">SP</span>
+          </div>
+        </div>
+        <div className="text-xl">
+          Magic Point:{" "}
+          <div className="inline-flex items-center justify-center">
+            {parseFloat(formatEther(mp || 0n))}
+            <span className="font-bold ml-1">MP</span>
+          </div>
+        </div>
+        <div className="text-xl">
+          Defense Point:{" "}
+          <div className="inline-flex items-center justify-center">
+            {parseFloat(formatEther(dp || 0n))}
             <span className="font-bold ml-1">DP</span>
-          </p>
+          </div>
         </div>
       </div>
     </div>
