9500d734a43681673b36daf157c72f34ef31c770
Codechef Song <133816663+codechefsong@users.noreply.github.com>
2024-06-29 16:18:44 -0400

    update stake Rune Tree page

--- packages/nextjs/app/runetree/stake/page.tsx
+++ packages/nextjs/app/runetree/stake/page.tsx
@@ -34,58 +34,63 @@ const RuneTree: NextPage = () => {
           <p className="text-gray-500">Other can attack your Rune Tree to take your Rune Credits</p>
         </div>
 
-        <div className="flex flex-col space-y-2">
-          <IntegerInput
-            placeholder="Amount of RC to Stake"
-            value={amountToStake.toString()}
-            onChange={value => setAmountToStake(value)}
-            disableMultiplyBy1e18
-          />
-        </div>
+        {runeTrees?.amount === BigInt(0) ? (
+          <>
+            <div className="flex flex-col space-y-2">
+              <IntegerInput
+                placeholder="Amount of RC to Stake"
+                value={amountToStake.toString()}
+                onChange={value => setAmountToStake(value)}
+                disableMultiplyBy1e18
+              />
+            </div>
+            <button
+              className="py-2 px-16 mb-1 mt-3 bg-green-500 rounded baseline hover:bg-green-300 disabled:opacity-50"
+              onClick={async () => {
+                try {
+                  await Game({
+                    functionName: "stakeAndCreateRuneTree",
+                    args: [parseEther(amountToStake.toString())],
+                  });
+                } catch (e) {
+                  console.error("Error staking and creating Rune Tree:", e);
+                }
+              }}
+            >
+              Stake
+            </button>
+          </>
+        ) : (
+          <button
+            className="py-2 px-16 mb-1 mt-3 bg-green-500 rounded baseline hover:bg-green-300 disabled:opacity-50"
+            onClick={async () => {
+              try {
+                await Game({
+                  functionName: "collectCredits",
+                  args: [address],
+                });
+              } catch (e) {
+                console.error("Error collection credits:", e);
+              }
+            }}
+          >
+            Claim Credit
+          </button>
+        )}
 
-        <button
-          className="py-2 px-16 mb-1 mt-3 bg-green-500 rounded baseline hover:bg-green-300 disabled:opacity-50"
-          onClick={async () => {
-            try {
-              await Game({
-                functionName: "stakeAndCreateRuneTree",
-                args: [parseEther(amountToStake.toString())],
-              });
-            } catch (e) {
-              console.error("Error staking and creating Rune Tree:", e);
-            }
-          }}
-        >
-          Stake
-        </button>
-
-        <button
-          className="py-2 px-16 mb-1 mt-3 bg-green-500 rounded baseline hover:bg-green-300 disabled:opacity-50"
-          onClick={async () => {
-            try {
-              await Game({
-                functionName: "collectCredits",
-                args: [address],
-              });
-            } catch (e) {
-              console.error("Error collection credits:", e);
-            }
-          }}
-        >
-          Claim Credit
-        </button>
-
-        <div>
-          <h2 className="mt-3 text-2xl">Your Rune Tree</h2>
-          <p className="mr-2">
-            Amount: {parseFloat(formatEther(runeTrees?.amount || 0n))}
-            <span className="font-bold ml-1">RC</span>
-          </p>
-          <p className="mr-2">
-            Defense Point: {parseFloat(formatEther(runeTrees?.defensePoint || 0n))}
-            <span className="font-bold ml-1">DP</span>
-          </p>
-        </div>
+        {runeTrees?.amount !== BigInt(0) && (
+          <div>
+            <h2 className="mt-3 text-2xl">Your Rune Tree</h2>
+            <p className="mr-2">
+              Amount: {parseFloat(formatEther(runeTrees?.amount || 0n))}
+              <span className="font-bold ml-1">RC</span>
+            </p>
+            <p className="mr-2">
+              Defense Point: {parseFloat(formatEther(runeTrees?.defensePoint || 0n))}
+              <span className="font-bold ml-1">DP</span>
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
