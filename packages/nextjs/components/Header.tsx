f3d842e34688bb822cbc7f3fa500760c8348aeb7
Codechef Song <133816663+codechefsong@users.noreply.github.com>
2024-06-30 14:02:35 -0400

    add logo

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -112,8 +112,7 @@ export const Header = () => {
             />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Block Mage Minions</span>
-            <span className="text-xs"></span>
+            <Image src="/logo.jpg" width={150} height={150} alt="Logo" />
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
